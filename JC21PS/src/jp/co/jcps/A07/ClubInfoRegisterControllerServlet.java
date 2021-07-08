b48ea2fe61911f0b08d8318901ecee3c0469d1ad
yoshzawa <yoshzawa@gmail.com>
2021-07-08 12:21:03 +0900

    Revert "A07修正"
    This reverts commit 41de42762664f1944fc1ca867d0d0ad3a7cfb924.

--- JC21PS/src/jp/co/jcps/A07/ClubInfoRegisterControllerServlet.java
+++ JC21PS/src/jp/co/jcps/A07/ClubInfoRegisterControllerServlet.java
@@ -1,6 +1,7 @@
 package jp.co.jcps.A07;
 
 import java.io.IOException;
+import java.sql.ResultSet;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -12,18 +13,21 @@ import javax.servlet.http.HttpServletResponse;
 
 import jp.co.jcps.Common.CommonCheck;
 import jp.co.jcps.Common.DBConnection;
+
 /**
  * 部活情報登録画面のコントローラー
  */
 @WebServlet("/ClubInfoRegisterController")
 public class ClubInfoRegisterControllerServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
+
 	/**
 	 * コンストラクタ
 	 */
 	public ClubInfoRegisterControllerServlet() {
 		super();
 	}
+
 	/**
 	 * GETメソッドでリクエストされた場合の処理
 	 */
@@ -34,17 +38,18 @@ public class ClubInfoRegisterControllerServlet extends HttpServlet {
 			// セッションが切れてる場合はログイン画面に遷移
 			request.getRequestDispatcher("/Login").forward(request, response);
 		}
+
 		// セッションからログイン中のユーザーの部長クラブIDを取得する
 		// TODO: セッション（ログインユーザーの情報を保持している）からログインユーザーの部長を務める部活の部活IDを取得する。
 
-
+		String leaderClubId = (String) request.getSession().getAttribute("leaderClubId");
 		// TODO:SQLを宣言
 
-
+		String sql = "SELECT club_name, club_description FROM mst_club WHERE club_id = ? ";
 		// TODO: SQLに埋め込むパラメータリストを定義
 		List<String> paramList = new ArrayList<String>();
 
-
+		paramList.add(leaderClubId);
 		// DB接続を初期化
 		DBConnection db = new DBConnection();
 
@@ -53,11 +58,11 @@ public class ClubInfoRegisterControllerServlet extends HttpServlet {
 
 		try {
 			// TODO; DBから情報を取得
-
+			ResultSet rs = db.executeSelectQuery(sql, paramList);
 			while (rs.next()) {
 				// TODO: beanにDBから取得した値をセット
-
-
+				bean.setClubName(rs.getString("club_name"));
+				bean.setClubDescription(rs.getString("club_description"));
 			}
 		} catch (Exception e) {
 			System.out.println(e.getMessage());
@@ -69,9 +74,12 @@ public class ClubInfoRegisterControllerServlet extends HttpServlet {
 				System.out.println(e.getMessage());
 			}
 		}
+
 		// beanをリクエストにセット
 		request.setAttribute("bean", bean);
+
 		// 部活情報登録画面を表示
 		request.getRequestDispatcher("A07/ClubInfoRegister.jsp").forward(request, response);
 	}
+
 }
