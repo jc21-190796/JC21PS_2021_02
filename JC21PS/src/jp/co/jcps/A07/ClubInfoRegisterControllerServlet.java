c217d1c923cc4a8cdb062aca91fe42f1b82d9383
yoshzawa <yoshzawa@users.noreply.github.com>
2021-07-18 12:14:10 +0900

    Update ClubInfoRegisterControllerServlet.java

--- JC21PS/src/jp/co/jcps/A07/ClubInfoRegisterControllerServlet.java
+++ JC21PS/src/jp/co/jcps/A07/ClubInfoRegisterControllerServlet.java
@@ -40,13 +40,11 @@ public class ClubInfoRegisterControllerServlet extends HttpServlet {
 		}
 
 		// セッションからログイン中のユーザーの部長クラブIDを取得する
-		// TODO: セッション（ログインユーザーの情報を保持している）からログインユーザーの部長を務める部活の部活IDを取得する。
 
 		String leaderClubId = (String) request.getSession().getAttribute("leaderClubId");
 		// TODO:SQLを宣言
 
 		String sql = "SELECT club_name, club_description FROM mst_club WHERE club_id = ? ";
-		// TODO: SQLに埋め込むパラメータリストを定義
 		List<String> paramList = new ArrayList<String>();
 
 		paramList.add(leaderClubId);
@@ -58,7 +56,6 @@ public class ClubInfoRegisterControllerServlet extends HttpServlet {
 		ClubInfoRegisterBean bean = new ClubInfoRegisterBean();
 
 		try {
-			// TODO; DBから情報を取得
 			ResultSet rs = db.executeSelectQuery(sql, paramList);
 			while (rs.next()) {
 				// TODO: beanにDBから取得した値をセット
